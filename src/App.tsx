dc5bd2e64f28de5362098af6adc4e0c009004389
Gabroz4 <gabriele.broccoli@gmail.com>
2025-11-12 01:47:49 +0100

    first wip deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 //import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 import { Button } from "@/components/ui/button" //si possono importare un sacco di altre cose con npx shadcn@latest add <component>
@@ -11,20 +10,11 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      
       <h1>Work in progress</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
       <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Tailwind button test</Button>
+      <Button>This button does nothing</Button>
       </div>
     </>
   )
